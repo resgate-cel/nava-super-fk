c1379b7d1dcdda0bbc223fb246765ed95e7ae937
resgate-cel <hassan14746193@gmail.com>
2026-04-16 14:13:44 -0300

    Update checkout-shared.js

--- js/checkout-shared.js
+++ js/checkout-shared.js
@@ -1,16 +1,16 @@
 const baseLinks = {
-    up1: "https://pay.finalize-certo.online/c/a79cf6bf-f9dd-403e-abe4-4cbf293c9587", // Verifique
-    up2: "https://pay.finalize-certo.online/c/1f7979ce-7088-4d9f-aa70-b3df2d66f6ca", // Prossiga
-    up3: "https://pay.finalize-certo.online/c/7625648b-dcab-4cfb-b81e-2827a8a30765", // Cadastro
-    up4: "https://pay.finalize-certo.online/c/51809fc0-a81e-4a87-a638-3441ebee42b4", // Pendente
-    up5: "https://pay.finalize-certo.online/c/27616470-daba-4cb7-a1bb-608df9da45fa", // Ativar Conta
-    up6: "https://pay.finalize-certo.online/c/338deeee-4474-4f73-b9b8-42838a2f32f4", // Registro
-    up7: "https://pay.finalize-certo.online/c/4c9fa61c-22ea-4ded-9062-e08afdebed05", // Acréscimo
-    up8: "https://pay.finalize-certo.online/c/579a3a86-8209-4740-835a-e3a4baea24be", // Documento
-    up9: "https://pay.finalize-certo.online/c/7d00895f-4a80-47b4-b987-c07d5d45f815", // Aplicativo
-    up10:"https://pay.finalize-certo.online/c/e051e320-b34d-4c08-aad3-0963840ed5a6", // Abertura
-    up11:"https://pay.finalize-certo.online/c/d93d88cf-e979-40eb-9823-a5fac14c22bc", // Consultoria
-    up12:"https://pay.finalize-certo.online/c/c6811cfb-f46f-4cf9-a2bc-be7bf33fe28b"  // Administrativo
+    up1: "https://pay.finalizeseupagamento.store/69d7d433c21e94c85f346994", // Verifique
+    up2: "https://pay.finalizeseupagamento.store/69d7d4d54a2524da6a777eb8", // Prossiga
+    up3: "https://pay.finalizeseupagamento.store/69d7ed61c21e94c85f348559", // Cadastro
+    up4: "https://pay.finalizeseupagamento.store/69d7edd0094b1616eaf48282", // Pendente
+    up5: "https://pay.finalizeseupagamento.store/69d7ee0dc21e94c85f3485d1", // Ativar Conta
+    up6: "https://pay.finalizeseupagamento.store/69d7ee50c21e94c85f3485ef", // Registro
+    up7: "https://pay.finalizeseupagamento.store/69d7eead4a2524da6a7798de", // Acréscimo
+    up8: "https://pay.finalizeseupagamento.store/69d7f6494a2524da6a77a0ed", // Documento
+    up9: "https://pay.finalizeseupagamento.store/69d7eee2b285a8a15edb772d", // Aplicativo
+    up10:"https://pay.finalizeseupagamento.store/69d7ef15c21e94c85f3486c6", // Abertura
+    up11:"https://pay.finalizeseupagamento.store/69d7ef45e1285315d41a700a", // Consultoria
+    up12:"https://pay.finalizeseupagamento.store/69d7ef76e1285315d41a7038"  // Administrativo
 };
 
 function redirect(key) {
